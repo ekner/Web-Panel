6a1305062fe85bc50e4b5cdc9e2b16f2fbdd0619
Gustav Ekner <gustav.ekner@gmail.com>
2016-04-09 21:15:13 +0200

    Added auto-reload by right-cliking the reload button. Also, placed all curly brackets on new lines (changed coding style)

--- web-panel-extension/panel.js
+++ web-panel-extension/panel.js
@@ -1,19 +1,25 @@
 var wpb; //web panel bookmarks folder id
 
-chrome.storage.local.get('lastSite', function(object) {
-  if ( typeof object.lastSite == "undefined") {
+chrome.storage.local.get('lastSite', function(object)
+{
+  if ( typeof object.lastSite === "undefined")
+  {
     chrome.storage.local.set({'lastSite': 'example.com'});
     $("#url").val("example.com");
     changeUrl();
-  } else {
+  }
+  else
+  {
     $("#url").val(object.lastSite);
     changeUrl();
   }
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender) {
+chrome.runtime.onMessage.addListener(function(message, sender)
+{
   // If the sender doesn't have a frame id, then we know it comes from the sidebar
-  if (message.fromCnt && !sender.frameId) {
+  if (message.fromCnt && !sender.frameId)
+  {
     $("#url").val(message.link);
     $("#loading").css("display", "none");
 
@@ -21,65 +27,87 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
   }
 });
 
-function changeUrl() {
+function changeUrl()
+{
   $("#loading").css("display", "block");
 
   var search = $("#url").val().match(/^[a-zA-Z]+:\/\//i);
 
-  if (search == null) {
+  if (search == null)
     $("#iframe").attr('src', "http://" + $("#url").val());
-  } else {
+  else
     $("#iframe").attr('src', $("#url").val());
-  }
 }
 
-$("#reload").click(function() {
+$("#reload").click(function()
+{
   changeUrl();
 });
 
-$("#url").keypress(function( event ) {
-  if ( event.which == 13 ) {
+$("#url").keypress(function( event )
+{
+  if ( event.which == 13 )
+  {
      event.preventDefault();
      changeUrl();
      return false;
   }
 });
 
-function createBookmark() {
-  if ($("#url").val() != "") {
+function createBookmark()
+{
+  if ($("#url").val() != "")
+  {
     var title = prompt("Bookmark title:", $("#url").val() );
-    if (title == "") {
+    if (title == "")
+    {
       alert("Please type a title for the bookmark. Press cancel on the next pop-up to escape.");
       createBookmark();
-    } else if (title != null) {
-      chrome.bookmarks.create({'parentId': wpb, 'url': $("#url").val(), 'title': title}, function(result) {
-        if (result === undefined) {
+    }
+    else if (title != null)
+    {
+      chrome.bookmarks.create({'parentId': wpb, 'url': $("#url").val(), 'title': title}, function(result)
+      {
+        if (result === undefined)
+        {
           alert("Bookmark not created: " + chrome.extension.lastError.message);
         }
       });
       loadBookmarks();
     }
-  } else {
+  }
+  else
+  {
     alert("You haven't entered a url.");
   }
 }
 
 $("#add-bookmark").click(createBookmark);
 
-chrome.bookmarks.search("Web Panel extension", function(list) {
-  if (typeof list[0] == "undefined") {
-    chrome.bookmarks.create({'title': 'Web Panel extension'}, function(folder) {
+chrome.bookmarks.search("Web Panel extension", function(list)
+{
+  if (typeof list[0] === "undefined")
+  {
+    chrome.bookmarks.create({'title': 'Web Panel extension'}, function(folder)
+    {
       wpb = folder.id;
       loadBookmarks();
     });
-  } else {
-    chrome.bookmarks.get(list[0].parentId, function(parent) {
-      if (parent[0].title == "Trash") {
-        chrome.bookmarks.create({'title': 'Web Panel extension'}, function(folder) {
+  }
+  else
+  {
+    chrome.bookmarks.get(list[0].parentId, function(parent)
+    {
+      if (parent[0].title == "Trash")
+      {
+        chrome.bookmarks.create({'title': 'Web Panel extension'}, function(folder)
+        {
           wpb = folder.id;
           loadBookmarks();
         });
-      } else {
+      }
+      else
+      {
         wpb = list[0].id;
         loadBookmarks();
       }
@@ -87,14 +115,21 @@ chrome.bookmarks.search("Web Panel extension", function(list) {
   }
 });
 
-function loadBookmarks() {
-  chrome.bookmarks.getChildren(wpb, function(result) {
+function loadBookmarks()
+{
+  chrome.bookmarks.getChildren(wpb, function(result)
+  {
     var content = "";
-    if (result.length == 0) {
+    if (result.length == 0)
+    {
       content = "<h3 style='margin-left: 10px;'>You have no bookmarks</h3>";
-    } else {
-      result.forEach(function(entry) {
-        if (typeof entry.url == "undefined") {
+    }
+    else
+    {
+      result.forEach(function(entry)
+      {
+        if (typeof entry.url === "undefined")
+        {
           return; // If it's a folder, skip it
         }
         var re = /(<([^>]+)>)/ig;
@@ -110,42 +145,165 @@ function loadBookmarks() {
     }
     $("#bookmarks-popup").html(content);
 
-    $(".box").on('contextmenu', function(e){
+    $(".box").on('contextmenu', function(e)
+    {
       e.preventDefault();
-      chrome.bookmarks.remove( $(this).attr("data-id"), function() {
+      chrome.bookmarks.remove( $(this).attr("data-id"), function()
+      {
         loadBookmarks();
       });
     });
-    $('.box').mousedown(function(event) {
-      if (event.which == 1) {
+    $('.box').mousedown(function(event)
+    {
+      if (event.which == 1)
+      {
         $("#url").val( $(this).attr("title") );
         changeUrl();
         fadeOut();
       }
     });
-
   });
 }
 
 var bookmarksPopupClosed = true;
 
-$("#bookmarks").click(function() {
-  if (bookmarksPopupClosed) {
+$("#bookmarks").click(function()
+{
+  if (bookmarksPopupClosed)
     fadeIn();
-  } else {
+  else
     fadeOut();
-  }
 });
 
-function fadeIn() {
+function fadeIn()
+{
   //$("#bookmarks-popup").css("display", "block");
   $("#bookmarks-popup").fadeIn(100);
   bookmarksPopupClosed = false;
 }
 
-function fadeOut() {
+function fadeOut()
+{
   $("#bookmarks-popup").fadeOut(100, function() {
     //$("#bookmarks-popup").css("display", "none");
   });
   bookmarksPopupClosed = true;
 }
+
+/* Auto-refresh: */
+
+var displayAutoReload = true;
+var autoReload = false;
+
+function openAutoReload()
+{
+  displayAutoReload = false;
+      
+  $("#auto-reload").css("left", event.pageX);
+  $("#auto-reload").css("top", event.pageY);
+  
+  $("#auto-reload").css("display", "block");
+}
+
+function closeAutoReload()
+{
+  displayAutoReload = true;
+  $("#auto-reload").css("display", "none");
+}
+
+$("#reload").bind("contextmenu", function (event)
+{
+  event.preventDefault();
+
+  if (displayAutoReload)
+    openAutoReload();
+  else
+    closeAutoReload();
+});
+
+// The user should also be able to close with left click:
+$("#reload").click(function()
+{
+  if (!displayAutoReload)
+    closeAutoReload();
+});
+
+// And by pressing "close":
+$("#auto-reload .close").click(function()
+{
+  if (!displayAutoReload)
+    closeAutoReload();
+});
+
+function setReload(time, item)
+{
+  removeReload();
+  
+  autoReload = setInterval(function()
+  {
+    changeUrl();
+  },
+  time * 1000);
+  
+  $(item).css("color", "lightblue");
+  $("#reload").css("background-color", "lightblue");
+  $("#auto-reload .clear").css("display", "block");
+}
+
+function removeReload()
+{
+  $("#auto-reload li").css("color", "black");
+  $("#reload").css("background-color", "transparent");
+  $("#auto-reload .clear").css("display", "none");
+  
+  if (autoReload != false)
+    clearInterval(autoReload);
+    
+  closeAutoReload();
+}
+
+$("#auto-reload li").click(function()
+{
+  // The Value the user clicked on on the list:
+  var item = this;
+  var time = Number( $(this).attr("data-time") );
+  
+  // Security, if the user has modified the HTML:
+  if (isNaN(time))
+    return;
+  
+  if (time != 0)
+  {
+    setReload(time, item);
+  }
+  else
+  {
+    var lastCustomTime = "";
+    chrome.storage.local.get('lastCustomTime', function(object)
+    {
+      if ( typeof object.lastCustomTime !== "undefined")
+        lastCustomTime = object.lastCustomTime;
+    });
+    
+    time = "";
+    var wrong = "";
+    while (time != null && time.match(/^\d+:\d+:\d+$/) == null || time == "0:0:0")
+    {
+      time = prompt(wrong + "Please enter the interval in this format: Hours:Minutes:Seconds", lastCustomTime);
+      wrong = "Wrong format specified.\n\n";
+    }
+    // If the user has pressed cancel on the prompt:
+    if (time == null)
+      return;
+    
+    chrome.storage.local.set({'lastCustomTime': time});
+    var values = time.split(":");
+    time = Number(values[0]) * 3600 + Number(values[1]) * 60 + Number(values[2]);
+    setReload(time, item);
+  }
+});
+
+$("#auto-reload .clear").click(function()
+{
+  removeReload();
+});
