151077dfb0e4fc2abde048a5a4c1963c6bf79ae6
Gustav Ekner <gustav.ekner@gmail.com>
2016-08-09 16:05:05 +0200

    Fixed coding style

--- web-panel-extension/background.js
+++ web-panel-extension/background.js
@@ -1,10 +1,14 @@
 // This removes x-frame restrictions on some sites (not all):
-chrome.webRequest.onHeadersReceived.addListener(
-  function(info) {
+chrome.webRequest.onHeadersReceived.addListener
+(
+  function(info)
+  {
     var headers = info.responseHeaders;
-    for (var i=headers.length-1; i>=0; --i) {
+    for (var i = headers.length - 1; i >= 0; --i)
+    {
       var header = headers[i].name.toLowerCase();
-      if (header == 'x-frame-options' || header == 'frame-options' || header == 'content-security-policy' || header == 'x-content-type-options' || header == 'x-xss-protection') {
+      if (header == 'x-frame-options' || header == 'frame-options' || header == 'content-security-policy' || header == 'x-content-type-options' || header == 'x-xss-protection')
+      {
         headers.splice(i, 1); // Remove header
       }
     }
