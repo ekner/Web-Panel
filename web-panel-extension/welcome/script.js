e5f155f8cddef438a09f07393180865308f2ff03
Gustav Ekner <gustav.ekner@gmail.com>
2017-11-19 02:22:03 +0100

    Fixed dark theme for startup page #12

---
+++ web-panel-extension/welcome/script.js
@@ -0,0 +1,20 @@
+function setTheme(data)
+{
+	if (typeof data.theme === 'undefined') {
+		data.theme = 'light';
+	}
+
+	if (data.theme === 'dark' || data.theme === 'dark-classic')
+		$('#theme-link').attr('href', 'dark.css');
+	else if (data.theme === 'light' || data.theme === 'light-classic')
+		$('#theme-link').attr('href', '');
+}
+
+function reloadTheme(message, sender, response)
+{
+	if (typeof message.msg !== 'undefined' && message.msg === 'reloadTheme')
+		chrome.storage.local.get('theme', function(data) { setTheme(data) });
+}
+
+chrome.storage.local.get('theme', function(data) { setTheme(data) });
+chrome.runtime.onMessage.addListener(function(message, sender, response) { reloadTheme(message, sender, response); });
